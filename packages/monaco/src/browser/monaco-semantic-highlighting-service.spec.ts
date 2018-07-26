e88ed90accf566bf2d917e2526691acecc87b035
Akos Kitta <kittaakos@typefox.io>
2018-07-26 16:32:02 +0200

    GH-1845: Added first text model tests.
    Signed-off-by: Akos Kitta <kittaakos@typefox.io>

---
+++ packages/monaco/src/browser/monaco-semantic-highlighting-service.spec.ts
@@ -0,0 +1,58 @@
+/********************************************************************************
+ * Copyright (C) 2018 TypeFox and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ ********************************************************************************/
+
+import Uri from 'vscode-uri';
+import { expect } from 'chai';
+import LanguageIdentifier = monaco.services.LanguageIdentifier;
+import ITextModel = monaco.editor.ITextModel;
+
+const { Range } = require('esm')(module)('monaco-editor-core/esm/vs/editor/common/core/range');
+const TextModel: {
+    createFromString(text: string, options?: ITextModelCreationOptions, languageIdentifier?: LanguageIdentifier, uri?: Uri): ITextModel
+} = require('esm')(module)('monaco-editor-core/esm/vs/editor/common/model/textModel').TextModel;
+
+enum DefaultEndOfLine {
+    LF = 1,
+    CRLF = 2
+}
+
+interface ITextModelCreationOptions {
+    tabSize: number;
+    insertSpaces: boolean;
+    detectIndentation: boolean;
+    trimAutoWhitespace: boolean;
+    defaultEOL: DefaultEndOfLine;
+    isForSimpleWidget: boolean;
+    largeFileOptimizations: boolean;
+}
+
+describe('monaco-semantic-highlighting-service', () => {
+
+    it('can create a model', () => {
+        const model = TextModel.createFromString(`const a = 'foo';`);
+        expect(model.getValue()).to.be.equal(`const a = 'foo';`);
+    });
+
+    it('can modify a model', () => {
+        const model = TextModel.createFromString(`const a = 'foo';`);
+        model.applyEdits([{
+            range: new Range(0, 0, 0, 0),
+            text: `/* comment */ `
+        }]);
+        expect(model.getValue()).to.be.equal(`/* comment */ const a = 'foo';`);
+    });
+
+});
